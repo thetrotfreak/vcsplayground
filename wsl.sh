6fe1f7a7e937e7c2ad44bc575be7ea2cdc737344
Bivas Kumar <thetrotfreak@yahoo.com>
2023-07-04 13:14:27 +0530

    Update wsl.sh
    Don't sudo make install

--- wsl.sh
+++ wsl.sh
@@ -9,4 +9,4 @@ sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install build-essential cmake libboost-all-dev libtag1-dev zlib1g-dev ffmpeg -y
 
 git clone https://github.com/spotify/echoprint-codegen.git spotify-echoprint-codegen && cd spotify-echoprint-codegen/src
-sudo make install
+make install
